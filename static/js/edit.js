fe2e1845b76c87c665342180c815c634da393d0d
junrui93 <chenjunrui93@hotmail.com>
2016-12-19 15:57:35 +1100

    tinymce style fix

--- static/js/edit.js
+++ static/js/edit.js
@@ -27,12 +27,9 @@ $(function () {
     tinymce.init({ 
         selector:'textarea.mce',
         height: 400,
-        plugins: [
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table contextmenu paste code'
-        ],
-        toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+        menubar: false,
+        toolbar: 'undo redo | insert | styleselect | fontsizeselect bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | code | fullscreen',
+        fontsize_formats: "8px 10px 12px 14px 18px 24px 36px",
         file_picker_callback: function(callback, value, meta) {
             $('#img-upload-btn').click();
             upload(callback);
@@ -41,6 +38,11 @@ $(function () {
         init_instance_callback: function(editor) {
             editor.setContent($('#mce-initial-content').html());
         },
+        plugins: [
+            "advlist autolink lists link image charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table contextmenu paste imagetools"
+        ],
     });
 
     $('form').bind('form-pre-serialize', function(e) {
