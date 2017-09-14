c98cf80e318e5204a00298454e6371feecf928d9
junrui93 <chenjunrui93@hotmail.com>
2017-09-14 12:56:56 +1000

    change sqlalchemy code to orm style and add 2 columns in the database

--- db.py
+++ db.py
@@ -2,196 +2,201 @@ import os
 from datetime import datetime
 
 import yaml
-from sqlalchemy import create_engine, Table, Column, Integer, String, \
-    MetaData, DateTime, Boolean
-from sqlalchemy.sql import select, text
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 CWD = os.path.dirname(__file__)
 with open(os.path.join(CWD, 'config.yml'), 'r') as config_file:
     cfg = yaml.load(config_file)
 
-engine = create_engine('sqlite:///' + os.path.join(CWD, cfg['sqlite']['db']), echo=True)
+ENGINE = create_engine('sqlite:///' + os.path.join(CWD, cfg['sqlite']['db']), echo=True)
 
-metadata = MetaData()
-
-partner = Table(
-    'partner', metadata,
-    Column('id', Integer, primary_key=True),
-    Column('logo_url', String, nullable=False),
-    Column('html', String, nullable=False),
-    Column('published_time', DateTime, nullable=False, default=datetime.now()),
-    Column('modified_time', DateTime, nullable=False, default=datetime.now(),
-           onupdate=datetime.now()),
-    Column('hidden', Boolean, nullable=False, default=False)
-)
-
-activity = Table(
-    'activity', metadata,
-    Column('id', Integer, primary_key=True),
-    Column('title', String, nullable=False),
-    Column('html', String, nullable=False),
-    Column('thumbnail', String),
-    Column('activity_time', DateTime, nullable=False),
-    Column('published_time', DateTime, nullable=False, default=datetime.now()),
-    Column('modified_time', DateTime, nullable=False, default=datetime.now(),
-           onupdate=datetime.now()),
-    Column('hidden', Boolean, nullable=False, default=False)
-)
-
-hero = Table(
-    'hero', metadata,
-    Column('id', Integer, primary_key=True),
-    Column('image_url', String, nullable=False),
-    Column('description', String, nullable=True),
-)
-
-link = Table(
-    'link', metadata,
-    Column('id', Integer, primary_key=True),
-    Column('name', String, nullable=False),
-    Column('url', String, nullable=False),
-)
-
-misc = Table(
-    'misc', metadata,
-    Column('id', Integer, primary_key=True),
-    Column('key', String, nullable=False, unique=True),
-    Column('value', String, nullable=False),
-)
+Base = declarative_base()
 
 
-metadata.create_all(engine)
+class Partner(Base):
+    __tablename__ = 'partner'
+    id = Column(Integer, primary_key=True)
+    logo_url = Column(String, nullable=False)
+    html = Column(String, nullable=False)
+    published_time = Column(DateTime, nullable=False, default=datetime.now())
+    modified_time = Column(DateTime, nullable=False, default=datetime.now(), onupdate=datetime.now())
+    hidden = Column(Boolean, nullable=False, default=False)
+
+
+class Activity(Base):
+    __tablename__ = 'activity'
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    html = Column(String, nullable=False)
+    thumbnail = Column(String)
+    activity_time = Column(DateTime, nullable=False)
+    published_time = Column(DateTime, nullable=False, default=datetime.now())
+    modified_time = Column(DateTime, nullable=False, default=datetime.now(), onupdate=datetime.now())
+    hidden = Column(Boolean, nullable=False, default=False)
+    is_link = Column(Boolean, nullable=False, default=False)
+    link = Column(String, default=None)
+
+
+class Hero(Base):
+    __tablename__ = 'hero'
+    id = Column(Integer, primary_key=True)
+    image_url = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+
+
+class Link(Base):
+    __tablename__ = 'link'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    url = Column(String, nullable=False)
+
+
+class Misc(Base):
+    __tablename__ = 'misc'
+    id = Column(Integer, primary_key=True)
+    key = Column(String, nullable=False, unique=True)
+    value = Column(String, nullable=False)
+
+
+Base.metadata.create_all(ENGINE)
+Session = sessionmaker(bind=ENGINE)
 
 
 def insert_partner(logo_url: str, html: str):
-    conn = engine.connect()
-    conn.execute(partner.insert(), logo_url=logo_url, html=html)
+    session = Session()
+    partner = Partner(logo_url=logo_url, html=html)
+    session.add(partner)
+    session.commit()
 
 
 def select_partner_all(hidden=False):
-    conn = engine.connect()
-    stmt = select([partner])
+    session = Session()
+    query = session.query(Partner)
     if hidden is not None:
-        stmt = stmt.where(partner.c.hidden == hidden)
-    return conn.execute(stmt).fetchall()
+        query = query.filter_by(hidden=hidden)
+    return query.all()
 
 
 def select_partner_by_id(partner_id: int):
-    conn = engine.connect()
-    stmt = select([partner]).where(partner.c.id == partner_id)
-    return conn.execute(stmt).fetchone()
+    session = Session()
+    return session.query(Partner).get(partner_id)
 
 
 def update_partner(partner_id: int, param: dict):
-    conn = engine.connect()
-    stmt = partner.update().values(param).where(partner.c.id == partner_id)
-    conn.execute(stmt)
+    session = Session()
+    session.query(Partner).filter_by(id=partner_id).update(param)
+    session.commit()
 
 
 def toggle_partner(partner_id: int):
-    conn = engine.connect()
-    stmt = text("UPDATE partner SET hidden = NOT hidden WHERE id = :partner_id")
-    stmt = stmt.bindparams(partner_id=partner_id)
-    conn.execute(stmt)
+    session = Session()
+    partner = session.query(Partner).get(partner_id)
+    partner.hidden = not partner.hidden
+    session.commit()
 
 
 def remove_partner(partner_id: int):
-    conn = engine.connect()
-    stmt = partner.delete().where(partner.c.id == partner_id)
-    conn.execute(stmt)
+    session = Session()
+    session.query(Partner).filter_by(id=partner_id).delete()
+    session.commit()
 
 
 def insert_activity(title: str, html: str, thumbnail: str, activity_time: str):
-    conn = engine.connect()
-    conn.execute(activity.insert(), title=title, html=html, thumbnail=thumbnail,
-                 activity_time=activity_time)
+    session = Session()
+    activity = Activity(title=title, html=html, thumbnail=thumbnail, activity_time=activity_time)
+    session.add(activity)
+    session.commit()
 
 
 def select_activity_all(hidden=False):
-    conn = engine.connect()
-    stmt = select([activity]).order_by(activity.c.activity_time.desc())
+    session = Session()
+    query = session.query(Activity)
     if hidden is not None:
-        stmt = stmt.where(activity.c.hidden == hidden)
-    return conn.execute(stmt).fetchall()
+        query = query.filter_by(hidden=hidden)
+    return query.order_by(Activity.activity_time.desc()).all()
 
 
 def select_activity_limit(limit=4):
-    conn = engine.connect()
-    stmt = select([activity]).where(activity.c.hidden == False).order_by(activity.c.activity_time.desc()).limit(limit)
-    return conn.execute(stmt).fetchall()
+    session = Session()
+    return session.query(Activity).filter_by(hidden=False).order_by(Activity.activity_time.desc())[:limit]
 
 
 def select_activity_by_id(activity_id: int):
-    conn = engine.connect()
-    stmt = select([activity]).where(activity.c.id == activity_id)
-    return conn.execute(stmt).fetchone()
+    session = Session()
+    return session.query(Activity).get(activity_id)
 
 
 def update_activity(activity_id: int, param: dict):
-    conn = engine.connect()
-    stmt = activity.update().values(param).where(activity.c.id == activity_id)
-    conn.execute(stmt)
+    session = Session()
+    session.query(Activity).filter_by(id=activity_id).update(param)
+    session.commit()
 
 
 def toggle_activity(activity_id: int):
-    conn = engine.connect()
-    stmt = text("UPDATE activity SET hidden = NOT hidden "
-                "WHERE id = :activity_id")
-    stmt = stmt.bindparams(activity_id=activity_id)
-    conn.execute(stmt)
+    session = Session()
+    activity = session.query(Activity).get(activity_id)
+    activity.hidden = not activity.hidden
+    session.commit()
 
 
 def remove_activity(activity_id: int):
-    conn = engine.connect()
-    stmt = activity.delete().where(activity.c.id == activity_id)
-    conn.execute(stmt)
+    session = Session()
+    session.query(Activity).filter_by(id=activity_id).delete()
+    session.commit()
 
 
 def insert_hero(image_url: str, description: str):
-    conn = engine.connect()
-    conn.execute(hero.insert(), image_url=image_url, description=description)
+    session = Session()
+    hero = Hero(image_url=image_url, description=description)
+    session.add(hero)
+    session.commit()
 
 
 def select_hero_all():
-    conn = engine.connect()
-    stmt = select([hero])
-    return conn.execute(stmt).fetchall()
+    session = Session()
+    return session.query(Hero).all()
 
 
 def remove_hero(hero_id: int):
-    conn = engine.connect()
-    stmt = hero.delete().where(hero.c.id == hero_id)
-    conn.execute(stmt)
+    session = Session()
+    session.query(Hero).filter_by(id=hero_id).delete()
+    session.commit()
 
 
 def select_link_all():
-    conn = engine.connect()
-    stmt = select([link])
-    return conn.execute(stmt).fetchall()
+    session = Session()
+    return session.query(Link).all()
 
 
 def remove_link(link_id: int):
-    conn = engine.connect()
-    stmt = link.delete().where(link.c.id == link_id)
-    conn.execute(stmt)
+    session = Session()
+    session.query(Link).filter_by(id=link_id).delete()
+    session.commit()
 
 
 def insert_link(name: str, url: str):
-    conn = engine.connect()
-    conn.execute(link.insert(), name=name, url=url)
+    session = Session()
+    link = Link(name=name, url=url)
+    session.add(link)
+    session.commit()
 
 
 def get_misc(key: str):
-    conn = engine.connect()
-    stmt = select([misc]).where(misc.c.key == key)
-    item = conn.execute(stmt).fetchone()
+    session = Session()
+    item = session.query(Misc).filter_by(key=key).first()
     if item:
-        return item['value']
-    conn.execute(misc.insert(), key=key, value='')
+        return item.value
     return ''
 
 
 def set_misc(key: str, value: str):
-    conn = engine.connect()
-    stmt = misc.update().values(dict(value=value)).where(misc.c.key == key)
-    conn.execute(stmt)
+    session = Session()
+    item = session.query(Misc).filter_by(key=key).first()
+    if item:
+        item.value = value
+    else:
+        item = Misc(key=key, value=value)
+        session.add(item)
+    session.commit()
